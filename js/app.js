7c552381f27be694bed93018f8a56f8021589918
alenorgue <alenoriegag@gmail.com>
2025-03-26 21:05:54 +0100

    finished without bonus

--- js/app.js
+++ js/app.js
@@ -7,3 +7,28 @@ const pictures = [
 ];
 
 // buena suerte
+
+let index = 0;
+
+let slider = document.querySelector(".img-container");
+slider.style.backgroundImage = `url('./img/${pictures[index]}.jpeg')`;
+
+const buttonLeft = document.querySelector(".btn-left");
+
+const buttonRight = document.querySelector(".btn-right");
+
+buttonLeft.addEventListener("click", function () {
+  console.log("Flecha izquierda pulsada");
+  index--;
+if (index < 0) {index = pictures.length-1;} 
+  slider.style.backgroundImage = `url('./img/${pictures[index]}.jpeg')`;
+  
+});
+
+buttonRight.addEventListener("click", function () {
+  console.log("flecha derecha pulsada");
+  index++;
+  if (index >= pictures.length) {index = 0;} 
+  slider.style.backgroundImage = `url('./img/${pictures[index]}.jpeg')`;
+  
+});
